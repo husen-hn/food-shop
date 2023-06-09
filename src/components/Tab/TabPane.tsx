486cd0ebbb863a8b11edf0bd5eacf8dc71f49cab
Hossein HassanNejad <hosseinspell@gmail.com>
2023-06-09 18:58:20 +0330

    build debug

--- src/components/Tab/TabPane.tsx
+++ src/components/Tab/TabPane.tsx
@@ -6,8 +6,6 @@ type Props = {
     children: ReactElement | ReactElement[]
 }
 
-const TabPane = ({ title, children }: Props): JSX.Element => (
-    <div>{children}</div>
-)
+const TabPane = ({ children }: Props): JSX.Element => <div>{children}</div>
 
 export default TabPane
